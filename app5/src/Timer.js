4fbd169234f825ef1793bd21c017af31571d74de
dr-fet91 <dr.fet91@gmail.com>
2023-02-20 19:20:51 +0330

    lesson#11 - improve timer render method with move setInterval timer in componentDidMount method - react lifecycle
    improve timer render method with move setInterval timer in componentDidMount method - react lifecycle lesson 11

--- app5/src/Timer.js
+++ app5/src/Timer.js
@@ -1,26 +1,37 @@
 import React from 'react';
-
-class Timer extends React.Component{
-    constructor(){
-      super();
-      this.state = {
-        time : new Date().toLocaleTimeString(),
-      }
-    }
-    render(){
-      setInterval(()=>{
-        this.setState(
-          {
-            time: new Date().toLocaleTimeString(),
-          }
-        );
-      }, 1000);
-      return(
-        <h2 className='timer'>
-          it is {this.state.time}
-        </h2>
-      );
+var interval;
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
     }
   }
+  componentDidMount() {
+   interval = setInterval(() => {
+      this.setState(
+        {
+          time: new Date().toLocaleTimeString(),
+        }
+      );
+    }, 1000);
+  }
+  componentDidUpdate(){
+    if(this.state.time == '11:53:35 PM'){
+      clearInterval(interval);
+    }
+  }
+  componentWillUnmount(){
+    //
+  }
+  render() {
 
-  export default Timer;
+    return (
+      <h2 className='timer'>
+        it is {this.state.time}
+      </h2>
+    );
+  }
+}
+
+export default Timer;
