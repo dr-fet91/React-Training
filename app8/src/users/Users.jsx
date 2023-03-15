6c0a553ed641de59dbd11be8a9ecb429318bd601
dr-fet91 <dr.fet91@gmail.com>
2023-03-15 05:25:53 +0330

    lesson#35 - Acquaintance with https://jsonplaceholder.typicode.com/ & axios for transfer and get data from jsonplaceholder & install axios. get users with axios from jsonplaceholder
    Acquaintance with https://jsonplaceholder.typicode.com/ & axios for transfer and get data from jsonplaceholder & install axios. get users with axios from jsonplaceholder - lesson 35

--- app8/src/users/Users.jsx
+++ app8/src/users/Users.jsx
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import style from '../style.module.css'
 import swal from 'sweetalert';
+import axios from 'axios';
 
-const Users = ()=>{
+const Users = () => {
     const naigate = useNavigate();
-    const handleDelete = () =>{
-       // swal("Here's the title!", "...and here's the text!");
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+            setUsers(res.data);
+        }).catch(err => {
+            console.log(err);
+        });
+    }, []);
+
+    const handleDelete = () => {
+        // swal("Here's the title!", "...and here's the text!");
         swal({
             title: "Are you sure?",
             text: "Once deleted, you will not be able to recover this imaginary file!",
             icon: "warning",
             buttons: true,
             dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("Poof! Your imaginary file has been deleted!", {
-                icon: "success",
-              });
-            } else {
-              swal("Your imaginary file is safe!");
-            }
-          });
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal("Poof! Your imaginary file has been deleted!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
     }
-    
+
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
             <h4 className="text-center">مدیریت کاربران</h4>
             <div className="row my-2 mb-4 justify-content-between w-100 mx-0">
                 <div className="form-group col-10 col-md-6 col-lg-4">
-                    <input type="text" className="form-control shadow" placeholder="جستجو"/>
+                    <input type="text" className="form-control shadow" placeholder="جستجو" />
                 </div>
                 <div className="col-2 text-start px-0">
                     {/* در این حالت هم میشه پارامتر تحت عنوان استیت استفاده کرد */}
@@ -41,34 +52,40 @@ const Users = ()=>{
                     </Link>
                 </div>
             </div>
-            <table className="table bg-light shadow">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>نام</th>
-                        <th>نام کاربری</th>
-                        <th>ایمیل</th>
-                        <th>عملیات</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>qasem</td>
-                        <td>qasemB</td>
-                        <td>mahdicmptr@gmail.com</td>
-                        <td>
-                            
-                                <i onClick={()=>{
-                                    // در صورتی که بخوایم پارامتر های دیگه رو ارسال کنیم که توی یو ار ال هم نشون نده میشه یه آبجکت که مقدار استیت برابر اون چیزی که میخوایم باشه نوشت
-                                    // دقت کن حتما باید نامش استیت باشه
-                                    return naigate('/user/add/2', {state: {param1: 'param1', param2: 'param2'}});
-                                }} className="fas fa-edit text-warning mx-2 pointer"></i>
-                            <i className="fas fa-trash text-danger mx-2 pointer" onClick={()=>handleDelete(1)}></i>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            {users ? (
+                <table className="table bg-light shadow">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>نام</th>
+                            <th>نام کاربری</th>
+                            <th>ایمیل</th>
+                            <th>عملیات</th>
+                        </tr>
+                    </thead>
+                    {users.map(u => (
+                        <tbody key={u.id}>
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.username}</td>
+                                <td>{u.email}</td>
+                                <td>
+
+                                    <i onClick={() => {
+                                        return naigate('/user/add/2', { state: { param1: 'param1', param2: 'param2' } });
+                                    }} className="fas fa-edit text-warning mx-2 pointer"></i>
+                                    <i className="fas fa-trash text-danger mx-2 pointer" onClick={() => handleDelete(1)}></i>
+                                </td>
+                            </tr>
+                        </tbody>
+                    ))}
+
+                </table>
+            ) : (
+                <h4 className='text-info text-center'>موردی یافت نشد</h4>
+            )}
+
         </div>
     )
 
